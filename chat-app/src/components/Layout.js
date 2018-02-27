cdb99c79af108aea07ae1af4f63fd8abe55bf043
Pham Hoai An <Phamhoaianctu@gmail.com>
2018-02-27 19:03:53 +0700

    Fix constructor in Layout component

--- chat-app/src/components/Layout.js
+++ chat-app/src/components/Layout.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
-import io from 'socket.io-client';
+import io from 'socket.io-client'
+import TopMenu from './TopMenu';
+import ChatBody from './ChatBody';
 
-const socketUrl = "http://192.168.1.16:3231";
-export default class Layout extends Component{
-	
-	constructor(props){
-		super(props);
+const socketUrl = "http://localhost:3231"
+export default class Layout extends Component {
 
-		this.state = {
-			socket:null
-		}
+	constructor(props) {
+	  super(props);
+
+	  this.state = {
+	  	socket:null
+	  };
 	}
-	componentWillMount(){
-		this.initSocket();
+
+	componentWillMount() {
+		this.initSocket()
 	}
 
+	/*
+	*	Connect to and initializes the socket.
+	*/
 	initSocket = ()=>{
-		const socket = io(socketUrl);
+		const socket = io(socketUrl)
+
 		socket.on('connect', ()=>{
 			console.log("Connected");
 		})
 
-		this.setState({socket});
+		this.setState({socket})
 	}
-
-	render(){
-		return(
+	render() {
+		const { socket } = this.state
+		return (
 			<div className="chat_window">
+			<TopMenu />
+			<ChatBody socket={socket} />
 			</div>
 		);
 	}
