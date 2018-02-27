b6fee0da6c1fceaae90af69ca131a91f07cce2c2
Pham Hoai An <Phamhoaianctu@gmail.com>
2018-02-27 21:13:16 +0700

    fix bug Top menu

---
+++ chat-app/src/components/TopMenu.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+export default class TopMenu extends Component{
+  render(){
+    return(
+      <div>
+        <div className="top_menu">
+          <div className="button-group">
+            <div className="voice">
+              
+            </div>
+            <div className="voice">
+
+            </div>
+            <div className="voice">
+
+            </div>
+          </div>
+          <div className="title">Chat Application</div>
+          <div className="button-group-two">
+            <div className="voice">
+
+            </div>
+            <div className="voice">
+
+            </div>
+            <div className="voice">
+
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
