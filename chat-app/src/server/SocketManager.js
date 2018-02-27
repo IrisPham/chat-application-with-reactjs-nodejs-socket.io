41714cdb9cba06f2b97e4b0789918fa1de678dd8
Pham Hoai An <Phamhoaianctu@gmail.com>
2018-02-27 21:10:58 +0700

    Add method login for the users

--- chat-app/src/server/SocketManager.js
+++ chat-app/src/server/SocketManager.js
@@ -1,6 +1,30 @@
 const io = require('./index.js').io
 
+const { LOGIN} = require('../Events')
 
 module.exports = function(socket){
-	console.log("Socket Id:" + socket.id)
+
+	// console.log('\x1bc'); //clears console
+	console.log("Socket Id:" + socket.id);
+
+	socket.on(LOGIN,()=>{
+		io.emit(LOGIN,socket.id);
+	});
+
+	//Listen when the user sends the message
+  socket.on('newMessage', data => {
+  	//Resend messages to all users who are online.
+    console.log('data', data);
+    var myBox = {
+      mes: data,
+      userId: socket.id
+    };
+    socket.broadcast.emit('newMessage',myBox);
+  });
+
+	//User disconnects
+	socket.on('disconnect', ()=>{
+		io.emit(USER_DISCONNECTED, connectedUsers)
+		console.log("Disconnect", connectedUsers);
+	})
 }
